a6eb9256277a93db35b3dc2876a1294d87d47ef7
Miles Georgi <azimux@gmail.com>
2025-12-11 11:53:43 -0800

    Appease Rubocop

--- src/foobify_rails_app_config.rb
+++ src/foobify_rails_app_config.rb
@@ -4,13 +4,15 @@ module Foobara
       class FoobifyRailsAppConfig < Foobara::Model
         attributes do
           include_sample_command :boolean, default: true
-          use_rails_command_connector :boolean, default: false,
-                                                description: "Do you want to expose commands through the rails " \
-                                                             "router directly instead of running them in controller actions?"
-          use_active_record_type :boolean, default: false,
-                                           description: "Do you want to treat ActiveRecord classes like Foobara entities? " \
-                                                        "This means automatically casting primary keys to records and " \
-                                                        "being able to expose their attributes via command connectors"
+          use_rails_command_connector :boolean,
+                                      default: false,
+                                      description: "Do you want to expose commands through the rails " \
+                                                   "router directly instead of running them in controller actions?"
+          use_active_record_type :boolean,
+                                 default: false,
+                                 description: "Do you want to treat ActiveRecord classes like Foobara entities? " \
+                                              "This means automatically casting primary keys to records and " \
+                                              "being able to expose their attributes via command connectors"
           rspec :boolean, default: false
         end
 
