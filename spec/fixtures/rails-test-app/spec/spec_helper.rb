75550444b9206174be91eac0133c26c6cb29364b
Miles Georgi <azimux@gmail.com>
2025-12-13 21:03:14 -0800

    Setup rspec in rails-test-app

---
+++ spec/fixtures/rails-test-app/spec/spec_helper.rb
@@ -0,0 +1,13 @@
+ENV["RAILS_ENV"] = "test"
+ENV["FOOBARA_ENV"] = "test"
+
+require "bundler/setup"
+
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.disable_monkey_patching!
+  config.order = :defined
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+  config.raise_errors_for_deprecations!
+end
